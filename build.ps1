f5b08ad323ef4b2f228d03113c09764027e5d148
Francesco Carpanese <francescocarpanese@hotmail.it>
2023-05-31 23:47:08 +0200

    Added build

---
+++ build.ps1
@@ -0,0 +1,20 @@
+# Create conda environment
+$condaEnvName = "build_env"
+$condaCreateCmd = "conda create -n $condaEnvName python=3.10 -y"
+Invoke-Expression -Command $condaCreateCmd
+
+# Activate conda environment
+$activateEnvCmd = "conda activate $condaEnvName"
+Invoke-Expression -Command $activateEnvCmd
+
+# Install packages from requirements.txt
+$requirementsFile = "requirements.txt"
+$installRequirementsCmd = "pip install -r $requirementsFile"
+Invoke-Expression -Command $installRequirementsCmd
+
+# Run pyinstaller with the skimage location
+$pyInstallerCmd = @'
+pyinstaller --onefile ./main.py
+'@
+
+Invoke-Expression -Command $pyInstallerCmd
