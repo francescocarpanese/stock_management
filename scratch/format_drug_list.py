ef12a3a8c5801ae27f54263a8af7e3799a4ae4b3
Francesco Carpanese <francesco.carpanese@epfl.ch>
2025-11-05 08:38:29 +0100

    remove unneeded functions

--- scratch/format_drug_list.py
+++
@@ -1,8 +0,0 @@
-
-from stock_management.formatting_utils import normalize_text, process_file
-
-
-input_file = '/Users/francescocarpanese/Documents/stock_management/test_data/drug_list_raw.txt'
-output_file = '/Users/francescocarpanese/Documents/stock_management/assets/drug_list.txt'
-
-process_file(input_file, output_file)
